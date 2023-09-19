b89fb1a291e1353c7e7fe1fe22ff99d373a2a458
Leticia-23 <sleticiasanchez23@gmail.com>
2023-09-19 21:47:17 +0200

    configuration Config Server like slides

--- PizzeberusSpringCloud/config-server/src/main/java/com/hiberus/ApplicationConfigServer.java
+++ PizzeberusSpringCloud/config-server/src/main/java/com/hiberus/ApplicationConfigServer.java
@@ -2,11 +2,11 @@ package com.hiberus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
+// import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication(exclude={SolrAutoConfiguration.class})
-@EnableConfigServer
+@SpringBootApplication // (exclude={SolrAutoConfiguration.class})
+@EnableConfigServer // Enable config server
 public class ApplicationConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationConfigServer.class, args);
