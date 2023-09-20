9eb7015d7681fd183b6ed35fee90082aaa8d3910
Leticia-23 <sleticiasanchez23@gmail.com>
2023-09-20 13:12:11 +0200

    config Eureka

--- config-server/src/main/java/com/hiberus/ApplicationConfigServer.java
+++ config-server/src/main/java/com/hiberus/ApplicationConfigServer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication // (exclude={SolrAutoConfiguration.class})
-@EnableConfigServer // Enable config server
+@EnableConfigServer
 public class ApplicationConfigServer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationConfigServer.class, args);
