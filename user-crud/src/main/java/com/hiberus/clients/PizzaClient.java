e171d40b852c685b1c865dd7b0b583cc6b241058
Leticia-23 <sleticiasanchez23@gmail.com>
2023-09-26 13:27:27 +0200

    feign client works when pizza-read up and pizza exist

--- user-crud/src/main/java/com/hiberus/clients/PizzaClient.java
+++ user-crud/src/main/java/com/hiberus/clients/PizzaClient.java
@@ -4,10 +4,10 @@ import com.hiberus.dto.PizzaDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "pizzaRead")
 public interface PizzaClient {
-        @GetMapping(value = "/pizzaRead")
-        ResponseEntity<PizzaDto> checkFavPizzaForUser(@RequestParam Integer idPizza);
+        @GetMapping(value = "/pizzaRead/{id}")
+        ResponseEntity<PizzaDto> checkFavPizzaForUser(@PathVariable Integer id);
 }
